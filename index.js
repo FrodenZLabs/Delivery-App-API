b89519b0896f4cb1d502489458ee5562ede8eeb1
kibestevie <frodenzlabs@gmail.com>
2025-07-09 21:23:08 +0300

    Added mongo DB config 🛠️

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import {
+  MONGO_IP,
+  MONGO_PASSWORD,
+  MONGO_PORT,
+  MONGO_USER,
+} from "./config/config.js";
 dotenv.config({ quiet: true });
 
-const app = express();
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL)
+    .then(() => {
+      console.log("✅ Successfully connected to database");
+    })
+    .catch((err) => {
+      console.log("❌ Database error: ", err);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
+
+const app = express();
 app.use(express.json());
 
 app.get("/api/v1", (req, res) => {
