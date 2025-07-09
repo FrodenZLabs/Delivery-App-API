0ad3858a972383e6f74ea184740e7581129697fe
kibestevie <frodenzlabs@gmail.com>
2025-07-09 18:46:57 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config({ quiet: true });
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/v1", (req, res) => {
+  res.send("<h1>Hello world<h1/>");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`✅ Listening on port: ${port}`);
+});
